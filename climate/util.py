f2485ae0d8c4c3dd6a457249b40e2d8327b307dc
joejoezz <joejoezz@gmail.com>
2019-10-04 10:37:43 -0700

    numerous updates to plots/stats

--- climate/util.py
+++ climate/util.py
@@ -3,6 +3,8 @@ Climate utility functions
 """
 
 import pandas as pd
+from scipy.stats import genextreme as gev
+from scipy.stats import mode
 
 def get_config(config_path):
     """
@@ -34,6 +36,32 @@ def load_pdo():
     pdo = pd.DataFrame(index = pd.to_datetime(df['yyyymm'], format='%Y%m'), columns=['pdo'])
     pdo['pdo'] = df.pdo.values
     return pdo
+
+
+def get_gev_fit(data):
+    """
+    Tries GEV fits with several loc parameters, selects the best one
+    """
+    md = mode(data)[0][0]
+    std = np.std(data)
+    # first try with loc=mode
+    shape, loc, scale = gev.fit(data, loc=md)
+    # if bad try again with mean
+    if loc > md+std:
+        shape, loc, scale = gev.fit(data, loc=np.mean(data))
+    else:
+        print('GEV fit with mode')
+    # if still bad (ugh), try again with mode - std
+    if loc > md+std:
+        shape, loc, scale = gev.fit(data, loc=md-std)
+    else:
+        print('GEV fit with mean')
+    if loc > md+std:
+        print('GEV fit with c=0')
+        shape, loc, scale = gev.fit(data, 0)
+    else:
+        print('GEV fit with mode minus std deviation')
+    return shape, loc, scale
 
 
 
